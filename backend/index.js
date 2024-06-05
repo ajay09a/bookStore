3ed5d4bdbff00797b06de567c192ac1f51bddd5c
Ajay <92861422+ajay09a@users.noreply.github.com>
2024-06-06 03:01:38 +0530

    create post method

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,39 @@
 import express from 'express';
 import { PORT, mongodbURL } from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 
 const app = express();
+//middleware for parsing body
+app.use(express.json());
 
 app.get('/', (req, res)=>{
-    console.log(req);
+    // console.log(req);
     return res.status(234).send("Welcome to mern stack!");
+})
+
+app.post('/books', async(req, res)=>{
+    try{
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ){
+            return res.status(400).send({
+                message: "Send all required field: title, author and publish year"
+            })
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear
+        }
+        const book  = await Book.create(newBook);
+    }
+    catch(error){
+        console.log(error.message);
+        return res.status(500).send({message: error.message})
+    }
 })
 
 mongoose.connect(mongodbURL)
