b7548fce7efae212ea94ef5684efb1198931a503
Ajay <92861422+ajay09a@users.noreply.github.com>
2024-06-06 12:04:36 +0530

    create get method

--- backend/index.js
+++ backend/index.js
@@ -29,10 +29,24 @@ app.post('/books', async(req, res)=>{
             publishYear: req.body.publishYear
         }
         const book  = await Book.create(newBook);
+        return res.status(201).send(book);
     }
     catch(error){
         console.log(error.message);
         return res.status(500).send({message: error.message})
+    }
+})
+
+app.get('/books', async(req, res)=>{
+    try {
+        const books = await Book.find({});
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
     }
 })
 
